4368751e393c284d22e39036ade606e6c2eb7332
Mohammad Sajjad Khan <aiservana24@gmail.com>
2026-01-21 05:57:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4380875989-418ed",
+  "appId": "1:989520788191:web:0102ccc4d756c4b161476a",
+  "apiKey": "AIzaSyDaVGaGy6jksZW9IoLv2XBX7ukygjbVrmk",
+  "authDomain": "studio-4380875989-418ed.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "989520788191"
+};
