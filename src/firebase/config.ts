f2575936e4d7376a44b57e4077d3248e88fc296e
Mohammad Sajjad Khan <aiservana24@gmail.com>
2026-01-23 04:06:15 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-4380875989-418ed",
   "appId": "1:989520788191:web:0102ccc4d756c4b161476a",
+  "storageBucket": "studio-4380875989-418ed.appspot.com",
   "apiKey": "AIzaSyDaVGaGy6jksZW9IoLv2XBX7ukygjbVrmk",
   "authDomain": "studio-4380875989-418ed.firebaseapp.com",
   "measurementId": "",
